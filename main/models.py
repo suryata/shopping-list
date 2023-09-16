94a85551e94eab311517c34c8564d9396ae3465a
suryata <madeputra415@gmail.com>
2023-09-16 08:10:00 +0700

    penambahan komen

--- main/models.py
+++ main/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+#model yang dibuat yaitu product
 class Product(models.Model):
     name = models.CharField(max_length=255)
     date_added = models.DateField(auto_now_add=True)
