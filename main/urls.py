dc051e71bb74d03aea7aab2062da539e6fc9f91e
suryata <madeputra415@gmail.com>
2023-10-04 13:34:43 +0700

    Tutorial 5 Selesai

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import delete_product,edit_product,show_main, create_product, show_xml, show_json, show_xml_by_id, show_json_by_id, register, login_user, logout_user
+from main.views import delete_product,edit_product,show_main, create_product, show_xml, show_json, show_xml_by_id, show_json_by_id, register, login_user, logout_user, get_product_json, add_product_ajax
 app_name = 'main'
 
 urlpatterns = [
@@ -14,4 +14,6 @@ urlpatterns = [
     path('logout/', logout_user, name='logout'),
     path('edit-product/<int:id>', edit_product, name='edit_product'),
     path('delete/<int:id>', delete_product, name='delete_product'),
+    path('get-product/', get_product_json, name='get_product_json'),
+    path('create-product-ajax/', add_product_ajax, name='add_product_ajax'),
 ]
