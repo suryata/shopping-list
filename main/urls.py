9fc5e0f6db0148a566b81fcfec326f6d0e57c6c9
suryata <madeputra415@gmail.com>
2023-11-15 14:50:57 +0700

    Tutorial 8

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
-from main.views import delete_product,edit_product,show_main, create_product, show_xml, show_json, show_xml_by_id, show_json_by_id, register, login_user, logout_user, get_product_json, add_product_ajax
-app_name = 'main'
+from main.views import *
 
 urlpatterns = [
     path('', show_main, name='show_main'),
@@ -16,4 +15,5 @@ urlpatterns = [
     path('delete/<int:id>', delete_product, name='delete_product'),
     path('get-product/', get_product_json, name='get_product_json'),
     path('create-product-ajax/', add_product_ajax, name='add_product_ajax'),
+    path('create-flutter/', create_product_flutter, name='create_product_flutter'),
 ]
